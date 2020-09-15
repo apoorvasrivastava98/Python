68935a71600c1cbedfb22aeac140c97e41b34678
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-09-15 20:40:17 +0530

    Create ArmstrongNoFunction.py

---
+++ ArmstrongNoFunction.py
@@ -0,0 +1,19 @@
+def armstrong(num):
+    n=num
+    sum=0
+    while (num > 0):
+        r=num%10
+        sum=sum+r*r*r
+        num=num/10
+        print(sum)
+        
+    if (n == sum):
+        print(sum)
+        print('Armstrong number')
+    else:
+        print(sum)
+        print('Not Armstrong number')
+        
+
+num = int(input("Enter a number: "))
+armstrong(num)
